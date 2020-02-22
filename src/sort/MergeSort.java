8ced24dee21174e69958c4570535691fa6ca49c1
Max <h.onto@yandex.ru>
2020-02-22 15:14:55 -0500

    LAB_2_improve test results

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -2,24 +2,20 @@ package sort;
 
 public class MergeSort extends AbstractSort
 {
-        public void Sort(Comparable[] source)
-        {
+        public void Sort(Comparable[] source) {
                 var m_new = new Comparable[source.length];
                 Sort(m_new , source, 0, source.length - 1);
-
-                Merge(m_new, source, 0, 0+source.length-1/2, source.length-1);
+                Merge(m_new, source, 0, source.length-1/2, source.length-1);
                 source = m_new;
         }
-        private void Sort(Comparable []arr, Comparable[] source, int low, int high)
-        {
+        private void Sort(Comparable []arr, Comparable[] source, int low, int high) {
                 if (high <= low) return;
                 int mid = low + (high - low) / 2;
                 Sort(arr, source, low, mid);
                 Sort(arr, source, mid + 1, high);
                 Merge(arr, source, low, mid, high);
         }
-        private void Merge(Comparable []arr, Comparable []source, int low, int mid, int high)
-        {
+        private void Merge(Comparable []arr, Comparable []source, int low, int mid, int high) {
                 int i = low, j=mid+1;
                 for (int k = low; k <= high; k++) arr[k]=source[k];
                 for (int k = low; k <= high; k++)
@@ -30,7 +26,6 @@ public class MergeSort extends AbstractSort
                         else source[k] = arr[i++];
                 }
         }
-
         @Override
         public String GetSortName() {
                 return "Merge sort";
--- src/sort/SortTest.java
+++ src/sort/SortTest.java
@@ -14,40 +14,34 @@ public class SortTest {
             throws InterruptedException{
         int ITERATIONS=10;
         var stats = new LinkedList<SortStatInfo>();
-        for(int i=1;i<=ITERATIONS;i++) {
-            System.out.println(String.format("PROCESSING %d/%d...",i,ITERATIONS));
+        for(int i=1;i<=ITERATIONS;i++)
             stats.add(GetAlgorithmStat(length, initializer, sort, millsPeriod));
-            System.out.println("DONE");
-            //FORCE TO COLLECT PREV TEST OBJECTS
-            System.gc();
-        }
         System.out.println(String.format("STAT INFO -> %s",sort.GetSortName()));
         var cpuUseAvg=0.0;
         var memAvgUseMB=0.0;
         var execAvgTime = 0.0;
         for (var stat:stats) {
             cpuUseAvg += stat.GetCPUUsagePercent();
-            memAvgUseMB += stat.GetUsagedMemoryMB();
+            memAvgUseMB += stat.GetUsagedMemoryKB();
             execAvgTime += stat.GetExecedTime();
         }
         cpuUseAvg /= ITERATIONS;
         memAvgUseMB /= ITERATIONS;
         execAvgTime /= ITERATIONS;
         System.out.printf("ARRAY LENGTH %d\n", stats.get(0).GetSourceLenght());
-        System.out.printf("AVG EXEC TIME:%.2f\nAVG CPU USAGE(%%):%.2f\nAVG MEM USAGE (MB):%.2f\n",
-                execAvgTime,
-                cpuUseAvg,
-                memAvgUseMB/1000
-        );
+        System.out.printf("AVG EXEC TIME:%.2f\nAVG CPU USAGE(%%):%.2f\nAVG MEM USAGE (KB):%.2f\n",
+                execAvgTime, cpuUseAvg, memAvgUseMB);
     }
-    private static SortStatInfo GetAlgorithmStat(int length, IArrayInitializer initializer, AbstractSort sort, int millsPeriod){
+    private static SortStatInfo GetAlgorithmStat(int length, IArrayInitializer initializer,
+                                                 AbstractSort sort, int millsPeriod){
         if(initializer==null) throw new NullPointerException("Initializer null");
         if(sort==null) throw new NullPointerException("Sort null");
 
         var statInfo = new SortStatInfo();
         var timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
-            OperatingSystemMXBean operatingSystemMXBean = (OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean();
+            OperatingSystemMXBean operatingSystemMXBean = (OperatingSystemMXBean)
+                    ManagementFactory.getOperatingSystemMXBean();
             @Override
             public void run() {
                 var cpu = operatingSystemMXBean.getProcessCpuLoad()*100;
@@ -56,13 +50,9 @@ public class SortTest {
             }
         }, 0, millsPeriod);
         var array = initializer.GetInitializedArray(length);
-        var sortThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                sort.Sort(array);
-            }
-        });
+        var sortThread = new Thread(() -> sort.Sort(array));
         var startTime = Instant.now();
+        System.gc();
         sortThread.start();
         while(sortThread.isAlive());
         statInfo.UpdateExecTime(Duration.between(startTime,Instant.now()).toMillis());
@@ -71,81 +61,41 @@ public class SortTest {
         return statInfo;
     }
     public static void main(String[] args) throws InterruptedException {
-        for(int len=10;len<=Math.pow(10,6);len*=10) {
+        for(int len=10;len<=Math.pow(10,5);len*=10) {
             int finalLen = len;
             ShowAvgInfoAboutSort(finalLen,(i1) -> {
                 int MAX= finalLen;
                 var src = new Integer[MAX];
-                for(int i = 0; i <MAX; i++){
-                    src[i] = m_rand.nextInt(MAX);
-                }
+                for(int i = 0; i <MAX; i++) src[i] = m_rand.nextInt(MAX);
                 return src;
-            }, new MergeSort(), len/10);
+            }, new MergeSort(), 10);
         }
-        for(int len=100;len<=Math.pow(10,5);len*=10) {
+        for(int len=10;len<=Math.pow(10,5);len*=10) {
             int finalLen = len;
             ShowAvgInfoAboutSort(finalLen,(i1) -> {
                 int MAX= finalLen;
                 var src = new Integer[MAX];
-                for(int i = 0; i <MAX; i++){
-                    src[i] = m_rand.nextInt(MAX);
-                }
+                for(int i = 0; i <MAX; i++) src[i] = m_rand.nextInt(MAX);
                 return src;
-            }, new InsertionSort(), len/10);
+            }, new InsertionSort(), 10);
         }
-        for(int len=100;len<=Math.pow(10,5);len*=10) {
+        for(int len=10;len<=Math.pow(10,5);len*=10) {
             int finalLen = len;
             ShowAvgInfoAboutSort(finalLen,(i1) -> {
                 int MAX= finalLen;
                 var src = new Integer[MAX];
-                for(int i = 0; i <MAX; i++){
-                    src[i] = m_rand.nextInt(MAX);
-                }
+                for(int i = 0; i <MAX; i++) src[i] = m_rand.nextInt(MAX);
                 return src;
-            }, new SelectionSort(), len/10);
+            }, new SelectionSort(), 10);
         }
-        for(int len=100;len<=Math.pow(10,6);len*=10) {
+        for(int len=10;len<=Math.pow(10,5);len*=10) {
             int finalLen = len;
             ShowAvgInfoAboutSort(finalLen,(i1) -> {
                 int MAX= finalLen;
                 var src = new Integer[MAX];
-                for(int i = 0; i <MAX; i++){
-                    src[i] = m_rand.nextInt(MAX);
-                }
+                for(int i = 0; i <MAX; i++) src[i] = m_rand.nextInt(MAX);
                 return src;
-            }, new ShellSort(), len/10);
+            }, new ShellSort(), 10);
         }
     }
-}
-abstract class AlgorithmStatInfo {
-    private double _usageMemoryMB;
-    private double _cpuUsagePercent;
-    private double _execTime;
-    private int _updateCounter = 0;
-    public void UpdateMemAndCPU(double memMB, double cpuUsage){
-        _usageMemoryMB += memMB;
-        _cpuUsagePercent += cpuUsage;
-        _updateCounter++;
-    }
-    public void UpdateExecTime(double time){
-        _execTime = time;
-    }
-    public double GetExecedTime(){
-        return _execTime;
-    }
-    public double GetUsagedMemoryMB(){
-        return _updateCounter!=0?_usageMemoryMB/_updateCounter:0;
-    }
-    public double GetCPUUsagePercent(){ return _updateCounter!=0?_cpuUsagePercent/_updateCounter:0;}
-}
-class SortStatInfo extends AlgorithmStatInfo{
-    private int _srcLength;
-    public SortStatInfo(int length){
-        _srcLength = length;
-    }
-    public SortStatInfo() {
-        this(0);
-    }
-    public void SetSourceLenght(int length) { _srcLength = length; }
-    public int GetSourceLenght(){ return _srcLength; }
 }
